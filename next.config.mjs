fa47fbe925b3eb878efa17c62fd683bc94798bfa
ohara <kzAme79@gmail.com>
2024-07-08 23:00:21 +0900

    deploy settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/password-generator" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/password-generator/" : "",
+};
 
 export default nextConfig;
